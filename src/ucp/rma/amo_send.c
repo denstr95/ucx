842c3c65f9d8ef7028bdcc6b9ccbb54ad7079cef
denstr95 <denis-stroh@web.de>
2021-08-18 15:54:12 +0200

    Update amo_send.c

--- src/ucp/rma/amo_send.c
+++ src/ucp/rma/amo_send.c
@@ -188,7 +188,7 @@ UCS_PROFILE_FUNC(ucs_status_ptr_t, ucp_atomic_op_nbx,
         reply_buffer = NULL;
         op           = UCP_OP_ID_AMO_POST;
     }
-    printf("atomic hear!!!");
+    printf("atomic hear!!!/n");
     UCP_AMO_CHECK_PARAM_NBX(ep->worker->context, remote_addr, op_size,
                             count, opcode, UCP_ATOMIC_OP_LAST,
                             return UCS_STATUS_PTR(UCS_ERR_INVALID_PARAM));
@@ -202,7 +202,7 @@ UCS_PROFILE_FUNC(ucs_status_ptr_t, ucp_atomic_op_nbx,
                   param->cb.send : NULL);
   
      printf("atomic_op_nbx opcode %d buffer %p result %p "
-                  "datatype 0x%"PRIx64" remote_addr 0x%"PRIx64
+                  "datatype 0x%"PRIx64" remote_addr 0x%/n"PRIx64
                   " rkey %p to %s cb %p", opcode, buffer, reply_buffer,
                   param->datatype, remote_addr, rkey, ucp_ep_peer_name(ep),
                   (param->op_attr_mask & UCP_OP_ATTR_FIELD_CALLBACK) ?
